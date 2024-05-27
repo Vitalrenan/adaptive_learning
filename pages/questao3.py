d4fe8a32ff8f483ff614a2888cc664953e6cef0c
Renan Luiz Andrade Vital <67766118+Vitalrenan@users.noreply.github.com>
2024-05-27 18:53:03 -0300

    Update questao3.py

--- pages/questao3.py
+++ pages/questao3.py
@@ -6,13 +6,18 @@ import preparation_teste
 import preparation_aula
 import treatments
 from POC_v2 import get_aula_rag, interacao_inicial, estrutura_bloom, conversa
+from dotenv import load_dotenv
+import os
+import sqlite3
 
+#Setup 
+load_dotenv()
 
 st.set_page_config(layout="wide")
 st.title("Feedback personalizado de compreensão de conteúdo")
 
 #caching on browser
-@st.cache_data
+#@st.cache_data
 def update_history():   
     conversation = st.session_state.messages
     with open('conversation.pickle', 'wb') as handle:
@@ -21,18 +26,17 @@ def update_history():
 @st.cache_data      
 def carregando_quiz(url_quiz,num_questao):
     payload={}
-    headers = {'Accept': 'application/json', 'x-secret': '148baeda-e1f2-11ec-8fea-0242ac120002' }
+    headers = {'Accept': 'application/json', 'x-secret': os.environ["ALEXANDRIA_SECRET"] }
     if url_quiz:
-        print('if')
         pass
     else:
-        print('else')
-        url_quiz='https://cms-api-kroton.platosedu.io/api/v1/external/questions?learningUnits[]=c43265d4-22e5-45d1-be50-cd3a8ce50b2c&bankType=endOfUnit&quantity=5'
+        token='c43265d4-22e5-45d1-be50-cd3a8ce50b2c'
+        url_quiz = f"https://cms-api-kroton.platosedu.io/api/v1/external/questions?learningUnits[]={token}&bankType=endOfUnit&quantity=5"
     quiz = requests.request("GET", url_quiz, headers=headers, data=payload)
     quiz=preparation_teste.read_teste(json.loads(quiz.text))
     learningUnit=quiz[0]['learningUnit']
     quiz = treatments.treat_quiz(quiz)
-
+    
     url_conteudo = f"https://cms-api-kroton.platosedu.io/api/v1/external/learning-units/{learningUnit}"
     aula = requests.request("GET", url_conteudo, headers=headers, data=payload)
     aula=preparation_aula.orquestrador(json.loads(aula.text))
@@ -40,10 +44,9 @@ def carregando_quiz(url_quiz,num_questao):
     questao = quiz[quiz['num_questao']==num_questao]['questao'].item() 
     alternas = quiz[quiz['num_questao']==num_questao]['alternativas'].item().split("',")
     alternas = [i.replace('[','').replace(']','').replace("'",'').replace(",",'').replace(r"\xa0",'') for i in alternas]
-    conteudo_relacionado=get_aula_rag(learningUnit=learningUnit, questao=questao)    
+    conteudo_relacionado=get_aula_rag(learningUnit=learningUnit, questao=questao) 
     return quiz, num_questao, questao, alternas, conteudo_relacionado
-        
-        
+
 #read previous conversation
 try:
     with open('conversation.pickle', 'rb') as conversation_pkl:
@@ -51,14 +54,17 @@ try:
 except:
     conversation=[]
 
-#Quiz setup
+#Sidebar
+with st.sidebar:
+    user_name = st.text_input('Insira seu nome:')
+    user_curso = st.selectbox("Curso", ("Engenharia Elétrica", 'Administração','Licenciatura','Psicologia'))
+#Page rendering
 col1, col2 = st.columns(spec=[0.6,0.4])
 with col1: 
     
     url_quiz = st.text_input('Insira a URL do quiz alexandria')
     try:    
         quiz, num_questao, questao, alternas, conteudo_relacionado = carregando_quiz(url_quiz,3)
-        print(questao)
         st.markdown(f'Questão {num_questao}: {questao}')
         sub_col1, sub_col2 = st.columns(spec=[0.7,0.4])
         with sub_col1:
@@ -77,14 +83,21 @@ with col2:
     reset_chat = st.button('Resetar histórico',use_container_width=True)   
     if conversation == []:
         st.session_state.messages = []
-        first_interaction = interacao_inicial()
+        first_interaction = interacao_inicial(user_name, user_curso)
         st.session_state.messages.append({"role": "assistant", "content": first_interaction})
+        
+        topicos_bloom, conversation = estrutura_bloom(questao, gabarito, feedback)
+        st.session_state.messages.append({"role": "assistant", "content": topicos_bloom['resposta_bloom']})
     elif reset_chat:
         st.session_state.messages = []
-        first_interaction = interacao_inicial()
+        first_interaction = interacao_inicial(user_name, user_curso)
         st.session_state.messages.append({"role": "assistant", "content": first_interaction})
+        
+        topicos_bloom, conversation = estrutura_bloom(questao, gabarito, feedback)
+        st.session_state.messages.append({"role": "assistant", "content": topicos_bloom['resposta_bloom']})
     else:
         st.session_state.messages=conversation
+        topicos_bloom, conversation = estrutura_bloom(questao, gabarito, feedback)
                 
     for n,message in enumerate(st.session_state.messages):
         with st.chat_message(message["role"]):
@@ -92,10 +105,9 @@ with col2:
             
     if resposta_aluno := st.chat_input("Responda aqui"):
         st.session_state.messages.append({"role": "user", "content": resposta_aluno})
-
-        topicos_bloom, conversation = estrutura_bloom(questao, gabarito, feedback)
-        completion = conversa(topicos_bloom['topicos_bloom'], topicos_bloom['resposta_bloom'], conteudo_relacionado, conversation, resposta_aluno)
-        #print(topicos_bloom['resposta_bloom'])
+        completion = conversa(
+            gabarito, topicos_bloom['resposta_bloom'], conteudo_relacionado,
+            conversation, resposta_aluno, user_name)
         st.session_state.messages.append({"role": "assistant", "content": f"{completion}"})
         update_history()
         st.rerun()
